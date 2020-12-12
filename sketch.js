99769469a6fc1ffc73267526fc6f88a2dc598c6b
ShivenAhuja <69977599+ShivenAhuja@users.noreply.github.com>
2020-12-12 19:42:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var ball;
+var position,database;
+
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    position=database.ref("ball/position")
+    position.on("value",readPosition,showError)
+}
+function readPosition(data){
+ position=data.val()
+ ball.x=position.x;
+ ball.y=position.y;
+}
+function writePosition(x,y){
+    database.ref('ball/position').set({
+       'x':position.x+x,
+       'y': position.y+y
+ })
+
+
+}
+
+function showError (){
+  console.log("errorConnecting to database")
+
+}
+
+function draw(){
+    background("white");
+    if (position!==undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+        drawSprites();
+
+    }
+  
+}
+
+function changePosition(x,y){
+    ball.x = ball.x + x;
+    ball.y = ball.y + y;
+}
